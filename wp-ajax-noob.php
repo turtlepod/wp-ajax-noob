b36e387fbe6d8696f18334325963f3c577eb6de2
David <david@shellcreeper.com>
2016-11-26 08:15:42 +0700

    1st

---
+++ wp-ajax-noob.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Plugin Name: WordPress AJAX Example for Beginners
+ * Plugin URI: https://shellcreeper.com/wp-ajax-for-beginners/
+ * Description: Example plugin from tutorial "WordPress AJAX for Beginners"
+ * Version: 1.0.0
+ * Author: David Chandra Purnama
+ * Author URI: https://shellcreeper.com/
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: wp-ajax-noob
+ * Domain Path: /languages/
+ *
+ * @author David Chandra Purnama <david@genbumedia.com>
+ * @copyright Copyright (c) 2016, Genbu Media
+**/
+if ( ! defined( 'WPINC' ) ) { die; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
