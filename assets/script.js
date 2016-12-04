93b911d5436cc5c5e4003692a1151f83d1404194
David <david@shellcreeper.com>
2016-12-04 11:02:32 +0700

    nonce.

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
 jQuery(document).ready(function($) {
 
 	$.ajax({
-		type: "POST",                 // use $_POST request to submit data
-		url: john_cena_ajax_url,      // URL to "wp-admin/admin-ajax.php"
+		type: "POST",                  // use $_POST request to submit data
+		url: john_cena_ajax_data.url, // URL to "wp-admin/admin-ajax.php"
 		data: {
 			action     : 'john_cena', // wp_ajax_*, wp_ajax_nopriv_*
+			nonce_ajax : john_cena_ajax_data.nonce,
+			nonce_data : $( '#john-cena' ).data( 'nonce' ),
 			first_name : 'John',      // PHP: $_POST['first_name']
 			last_name  : 'Cena',      // PHP: $_POST['last_name']
 		},
--- wp-ajax-noob.php
+++ wp-ajax-noob.php
@@ -43,8 +43,11 @@ function my_wp_ajax_noob_john_cena_shortcode_callback(){
 	/* Enqueue JS only if this shortcode loaded. */
 	wp_enqueue_script( 'my-wp-ajax-noob-john-cena-script' );
 
+	/* Create Nonce */
+	$nonce = wp_create_nonce( 'john-cena-shortcode' );
+
 	/* Output empty div. */
-	return '<div id="john-cena"></div>';
+	return '<div id="john-cena" data-nonce="' . esc_attr( $nonce ) . '"></div>';
 }
 
 
@@ -64,7 +67,15 @@ function my_wp_ajax_noob_scripts(){
 
 	/* JS + Localize */
 	wp_register_script( 'my-wp-ajax-noob-john-cena-script', $url . "assets/script.js", array( 'jquery' ), '1.0.0', true );
-	wp_localize_script( 'my-wp-ajax-noob-john-cena-script', 'john_cena_ajax_url', admin_url( 'admin-ajax.php' ) );
+
+	/* Localize Script Data */
+	$ajax_data = array(
+		'url'   => admin_url( 'admin-ajax.php' ),
+		'nonce' => wp_create_nonce( 'john-cena-script-nonce' ),
+	);
+
+	/* Send Data as JS var via Localize Script */
+	wp_localize_script( 'my-wp-ajax-noob-john-cena-script', 'john_cena_ajax_data', $ajax_data  );
 }
 
 
@@ -80,6 +91,14 @@ add_action( 'wp_ajax_nopriv_john_cena', 'my_wp_ajax_noob_john_cena_ajax_callback
  * Ajax Callback
  */
 function my_wp_ajax_noob_john_cena_ajax_callback(){
+
+	/* Check DOM nonce */
+	check_ajax_referer( 'john-cena-shortcode', 'nonce_data' );
+
+	/* Check Localize Script Nonce */
+	check_ajax_referer( 'john-cena-script-nonce', 'nonce_ajax' );
+
+	/* Get request */
 	$first_name = isset( $_POST['first_name'] ) ? $_POST['first_name'] : 'N/A';
 	$last_name = isset( $_POST['last_name'] ) ? $_POST['last_name'] : 'N/A';
 	?>
@@ -88,4 +107,9 @@ function my_wp_ajax_noob_john_cena_ajax_callback(){
 	<?php
 	wp_die(); // required. to end AJAX request.
 }
+
+
+
+
+
 
